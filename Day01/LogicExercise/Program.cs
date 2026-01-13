cfb9080afb1ec1014eb548a45c761bb4f68fe37a
zoppa <zoppa.c11@gmail.com>
2026-01-13 13:13:08 +0700

    refactor(Logic Exercise): change code to class namespace form

--- Day01/LogicExercise/Program.cs
+++ Day01/LogicExercise/Program.cs
@@ -1,12 +1,64 @@
-﻿// See https://aka.ms/new-console-template for more information
-// Console.WriteLine("Hello, World!");
-
-int n = 15;
-
-for (int i = 1; i <= n; i++)
+﻿namespace LogicExercise
 {
-    if ((i % 3 == 0) && (i % 5 == 0)) Console.WriteLine("foobar");
-    else if (i % 3 == 0) Console.WriteLine("foo");
-    else if (i % 5 == 0) Console.WriteLine("bar");
-    else Console.WriteLine(i);
+    class Program
+    {
+         static void Main(string[] args)
+        {
+            FooBarIteration();
+        }
+
+        static void FooBarIteration()
+        {
+            Console.WriteLine($"---Foobar Iteration---");
+
+            int n = InputInteger();
+            
+            Console.WriteLine($"Foobar for {n} iteration : ");
+            for (int i = 1; i <= n; i++)
+            {
+                switch (i)
+                {
+                    case int x when (x % 3 == 0) && (x % 5 == 0) :
+                        Console.WriteLine("foobar");
+                        break;
+
+                    case int x when x % 3 == 0 :
+                        Console.WriteLine("foo");
+                        break;
+
+                    case int x when x % 5 == 0 :
+                        Console.WriteLine("bar");
+                        break;
+
+                    default:
+                        Console.WriteLine(i);
+                        break;
+                }
+            }
+        }
+    
+        static int InputInteger()
+        {
+            int number = 1;
+            bool isValidInput = false;
+
+            Console.WriteLine("Please enter a number (ex : 15) : ");
+
+            while(!isValidInput)
+            {
+                string input = Console.ReadLine() ?? "";
+
+                if (int.TryParse(input, out number))
+                    {
+                        isValidInput = true;
+                    } 
+                else
+                    {
+                        Console.WriteLine("Invalid input. Please enter a valid number : ");
+                    }
+            }
+
+            return number;
+        }
+    }
 }
