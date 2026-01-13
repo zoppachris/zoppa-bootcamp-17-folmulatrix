9b7cb2fb751b9c33ebbe5f47de36a0d6a4d39eef
zoppa <zoppa.c11@gmail.com>
2026-01-13 10:42:52 +0700

    first commit and logic exercise

---
+++ Day01/LogicExercise/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+int n = 15;
+
+for (int i = 1; i <= n; i++)
+{
+    if ((i % 3 == 0) && (i % 5 == 0)) Console.WriteLine("foobar");
+    else if (i % 3 == 0) Console.WriteLine("foo");
+    else if (i % 5 == 0) Console.WriteLine("bar");
+    else Console.WriteLine(i);
+}
